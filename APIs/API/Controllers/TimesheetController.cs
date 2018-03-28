1b386fdb39755c7063e765d7743fcceb179ac1ff
Sid <siddharth.gupta@hotmail.com>
2018-03-28 03:31:31 -0400

    API work

---
+++ APIs/API/Controllers/TimesheetController.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/GeneralLiability")]
+    public class TimesheetController : ApiController
+    {
+
+        [Route("GetProjects")]
+        public List<Project> GetProjects(int clinetID)
+        {
+            return new List<Project> {
+                new Project { Id = 1001, Name = "Data Integration" },
+                new Project {Id=1002,Name="Data Research"}
+            };
+        }
+    }
+}
---
+++ UI/UI/jsCode/mytimesheet.js
@@ -0,0 +1,13 @@
+﻿function getProjects(clientId) {
+
+    $.ajax({
+        type: 'GET',
+        url: '/getProjects',
+        data: { clientId: clientId },
+        success: function (data) {
+            alert(data.d);
+        }
+
+    });     
+
+}
