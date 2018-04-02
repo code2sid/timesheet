771e6dd8a150fb78a4bbed1ba6dad796ce6e426c
code2sid <write2siddharthg@gmail.com>
2018-04-02 18:01:59 +0530

    check latest

--- UI/UI/jsCode/mytimesheet.js
+++ UI/UI/jsCode/mytimesheet.js
@@ -77,3 +77,16 @@ $(document).on('change', '#projects', function () {
     });
 });
 
+jQuery(function () {
+    var counter = 1;
+    jQuery('a.add-author').click(function (event) {
+        event.preventDefault();
+
+        var newRow = jQuery('<tr><td><input type="text" name="first_name' +
+            counter + '"/></td><td><input type="text" name="last_name' +
+            counter + '"/></td></tr>');
+        counter++;
+        jQuery('table.authors-list').append(newRow);
+
+    });
+});
