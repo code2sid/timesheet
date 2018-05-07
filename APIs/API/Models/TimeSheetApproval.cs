a127f0c52d741499625c9e839360909c9d386859
Sid <siddharth.gupta@hotmail.com>
2018-05-07 10:08:48 -0400

    Tables: TimeSheetApproval and UserTimeSheet.

---
+++ APIs/API/Models/TimeSheetApproval.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class TimeSheetApproval
+    {
+        public int Id { get; set; }
+        public int TimeSheetId { get; set; }
+        public int ApproverId { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public DateTime ApproverDate { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ APIs/API/Models/UserTimeSheet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class UserTimeSheet
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProjectId { get; set; }
+        public int TaskId { get; set; }
+        public DateTime FillDate { get; set; }
+        public int Hours { get; set; }
+        public bool IsSaved { get; set; }
+        public bool IsSubmitted { get; set; }
+        public string Comments { get; set; }
+    }
+}
